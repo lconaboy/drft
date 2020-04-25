fb666854323325dab7b5d9740a4cd303f7f7daff
Luke Conaboy <lukeconaboy@gmail.com>
2020-04-25 16:52:42 +0100

    Added generation of linear velocity and displacement fields

--- cosmology.py
+++ cosmology.py
@@ -1,13 +1,4 @@
-"""
-TODO
-- _power_spectrum
-  + fft_sample_spacing
-  + fft_sample_spacing_components
-
-- linear_velocity_ps
-"""
 import numpy as np
-
 
 class Cosmology:
     def __init__(self, a, params='planck2018'):
@@ -17,8 +8,9 @@ class Cosmology:
         useful cosmological quantities.
 
         :param: a (float) - scale factor to calculate values at
-        :param: params (str) - which set of cosmological parameters 
-                               to use
+        :param: params (str or list) - which set of cosmological parameters 
+                                       to use, either a string or list of
+                                       parameters from grafic header
         """
 
         # Check that the scale factor is realistic (this is for my
@@ -33,10 +25,11 @@ class Cosmology:
         # Calculate all the useful cosmological quantities at scale
         # factor a
         self.H0 = self.p['h'] * 100.0  # km/s/Mpc
-        self.E_a = self.E(self.a)
-        self.H_a = self.H()
-        self.D_a = self.D()
-        self.f_a = self.f()
+        self.E_a = self.E(self.a)  # H(a)/H0
+        self.H_a = self.H()  # Hubble parameter
+        self.D_a = self.D()  # growth factor
+        self.d_a = self.d()  # normalised growth factor
+        self.f_a = self.f()  # f(\Omega)
 
 
     def E(self, a):
@@ -69,6 +62,15 @@ class Cosmology:
         return 2.5 * self.p['omega_m'] * self.E_a * i 
 
 
+    def d(self):
+        """Growth factor normalised to 1 at z=0"""
+        from scipy.integrate import quad
+        i = quad(lambda _a: 1 / (_a * self.E(_a))**3.0, 1.0, 0.0)[0]
+        i *= 2.5 * self.p['omega_m'] * self.E_a
+        
+        return self.D_a / i
+
+    
     def f(self):
         """
         The equation for the growth factor as a function of scale factor,
@@ -110,247 +112,4 @@ class Cosmology:
                     'omega_l': params[1],
                     'h': params[2]}
 
-################################################################################
-###############################  OLD BELOW HERE  ###############################
-################################################################################
 
-def _fft_sample_spacing(N, boxsize):
-    """
-    Return the sample spacing in Fourier space, given some symmetric 3D box in real space
-    with N elements per dimension and length L.
-    See https://gitorious.org/bubble/szclusters/commit/da1402ef95f4d40c28f53f88c99bf079063308c7
-    """
-    kk = np.zeros([N, N, N], dtype=np.float32)
-    kx = np.zeros([N, N, N], dtype=np.float32)
-    ky = np.zeros([N, N, N], dtype=np.float32)
-    kz = np.zeros([N, N, N], dtype=np.float32)
-    kx, ky, kz = _fft_sample_spacing_components(N)
-    fac = (2. * np.pi / boxsize)
-    kk = np.sqrt(kx ** 2. + ky ** 2. + kz ** 2.) * fac
-    return kk
-
-def _fft_sample_spacing_components(N):
-    """
-    Return the sample spacing in Fourier space, given some symmetric 3D box in real space
-    with N elements per dimension and length L.
-    See https://gitorious.org/bubble/szclusters/commit/da1402ef95f4d40c28f53f88c99bf079063308c7
-    """
-    
-    NN = np.zeros(N, dtype=np.int32)
-    kx = np.zeros([N, N, N], dtype=np.float32)
-    ky = np.zeros([N, N, N], dtype=np.float32)
-    kz = np.zeros([N, N, N], dtype=np.float32)
-
-    NN = (N * np.fft.fftfreq(N, 1.)).astype(np.int32)
-    for i in range(N):
-        kx[NN[i], :, :] = NN[i]
-        ky[:, NN[i], :] = NN[i]
-        kz[:, :, NN[i]] = NN[i]
-    return kx, ky, kz
-
-
-def _window_function(N, p):
-    ''' Calculate CIC smoothing window function for PS estimation.
-    '''
-    W = np.zeros([N, N, N], dtype=np.float32)
-    kx = np.zeros([N, N, N], dtype=np.float32)
-    ky = np.zeros([N, N, N], dtype=np.float32)
-    kz = np.zeros([N, N, N], dtype=np.float32)
-    fp = float(p)
-    kny = N/2  # Nyquist frequency
-    kx, ky, kz = _fft_sample_spacing_components(N)
-    
-    #W = ( np.sinc( (np.pi*kx) / (2.*kny) ) * np.sinc( (np.pi*ky) / (2.*kny) ) * np.sinc( (np.pi*kz) / (2.*kny) ) )**p
-    W = ( np.sinc( np.pi*kx/2.*kny ) * np.sinc( np.pi*ky/2.*kny ) * np.sinc( np.pi*kz/2.*kny ) ) ** fp
-    return W
-
-def _cic_window_function(N):
-    ''' Calculate CIC smoothing window function for PS estimation.
-    '''
-    W = np.zeros([N, N, N], dtype=np.float32)
-    kx = np.zeros([N, N, N], dtype=np.float32)
-    ky = np.zeros([N, N, N], dtype=np.float32)
-    kz = np.zeros([N, N, N], dtype=np.float32)
-    kny = N/2  # Nyquist frequency
-    kx, ky, kz = _fft_sample_spacing_components(N)
-    
-    #W = ( np.sinc( (np.pi*kx) / (2.*kny) ) * np.sinc( (np.pi*ky) / (2.*kny) ) * np.sinc( (np.pi*kz) / (2.*kny) ) )**p
-    W = ( 1. - (2./3.) * np.sin((np.pi*kx)/(2.*kny))**2 ) * ( 1. - (2./3.) * np.sin((np.pi*ky)/(2.*kny))**2 ) * ( 1. - (2./3.) * np.sin((np.pi*kz)/(2.*kny))**2 )
-    return W
-
-
-def linear_velocity_ps(k, delta_k, **cosmo):
-    '''
-    Compute velocity power spectrum using equations in Iliev et al. 2007
-    k - wavenumber
-    delta_k - dimensionless density power spectrum
-    H0 - Hubble constant today
-    omegam0 - Total matter density today
-    omegal0 - Totel dark energy density today
-    z - redshift to compute velocity power spectrum
-    '''
-    # First, compute the growth factor
-    D_z_growth = D_z(**cosmo)
-    H0 = cosmo['h'] * 100.
-    # Compute E(z) = H(z)/H0 term
-    E_z = hzoverh0(**cosmo)
-    # Compute and return linear power spectrum
-    t1 = delta_k ** 2 / k ** 2
-    t2 = (9 * H0 ** 2 * cosmo['omega_M_0'] **
-          2 * (1. + cosmo['z']) ** 4) / (4 * E_z ** 2)
-    t3 = (1 - (5 / (3 * (1. + cosmo['z']) * D_z_growth))) ** 2
-
-    return np.sqrt(t1 * t2 * t3)
-
-
-def D_z(**cosmo):
-    """
-    Unnormalised linear growth factor
-    D(a) = 5 omegam / 2 H(a) / H(0) * integral[0:a] [da / [a H(a) H0]**3]
-    equation  from  peebles 1980 (or e.g. eq. 8 in lukic et al. 2008) """
-
-    import scipy.integrate
-
-    omegam0 = cosmo['omega_M_0']
-    omegal0 = cosmo['omega_lambda_0']
-
-    if (abs(omegam0 + omegal0 - 1.) > 1.e-4):
-        raise RuntimeError(
-            "Linear growth factors can only be calculated for flat cosmologies")
-
-    a = 1. / (1. + cosmo['z'])
-
-    # 1st calc. for z=z
-    lingrowth = scipy.integrate.quad(_lingrowthintegrand, 0., a, (omegam0))[0]
-    lingrowth *= 5. / 2. * omegam0 * hzoverh0(**cosmo)
-    return lingrowth
-
-
-def hzoverh0(**cosmo):
-    """ returns: H(a) / H0  = [omegam/a**3 + (1-omegam)]**0.5 """
-    return np.sqrt(cosmo['omega_M_0'] * np.power(cosmo['aexp'], -3) + (1. - cosmo['omega_M_0']))
-
-
-def _lingrowthintegrand(a, omegam):
-    """ (e.g. eq. 8 in lukic et al. 2008)   returns: da / [a*H(a)/H0]**3 """
-    return np.power((a * hzoverh0(**{'aexp': a, 'omega_M_0': omegam})), -3)
-
-
-def lingrowthfac(red, return_norm=False, **cosmo):
-    """
-    returns: linear growth factor, b(a) normalized to 1 at z=0, good for flat lambda only
-    a = 1/1+z
-    b(a) = Delta(a) / Delta(a=1)   [ so that b(z=0) = 1 ]
-    (and b(a) [Einstein de Sitter, omegam=1] = a)
-
-    Delta(a) = 5 omegam / 2 H(a) / H(0) * integral[0:a] [da / [a H(a) H0]**3]
-    equation  from  peebles 1980 (or e.g. eq. 8 in lukic et al. 2008) """
-    # need to add w ~= , nonflat, -1 functionality
-
-    import scipy.integrate
-
-    if (abs(cosmo['omega_M_0'] + cosmo['omega_lambda_0'] - 1.) > 1.e-4):
-        raise RuntimeError(
-            "Linear growth factors can only be calculated for flat cosmologies")
-
-    # 1st calc. for z=z
-    lingrowth = scipy.integrate.quad(
-        _lingrowthintegrand, 0., cosmo['aexp'], (cosmo['omega_M_0']))[0]
-    lingrowth *= 5. / 2. * cosmo['omega_M_0'] * \
-        hzoverh0(**cosmo)
-
-    # then calc. for z=0 (for normalization)
-    a0 = 1.
-    lingrowtha0 = scipy.integrate.quad(
-        _lingrowthintegrand, 0., a0, (cosmo['omega_M_0']))[0]
-    lingrowtha0 *= 5. / 2. * \
-        cosmo['omega_M_0'] * \
-        hzoverh0(**{'aexp': a0, 'omega_M_0': cosmo['omega_M_0']})
-
-    lingrowthfactor = lingrowth / lingrowtha0
-    if return_norm:
-        return lingrowthfactor, lingrowtha0
-    else:
-        return lingrowthfactor
-
-def compute_power(field, boxsize):
-    """Given a symmetric 3D field in real space, calculate the Fourier
-    transform and generate the power spectrum.
-
-    :param field:
-         (array) real field to calculate power spectrum of
-    :returns:     
-         (array) same shape as field, the 3D unbinned, unnormalised 
-         power spectrum of field
-         (array) length of one of field's axes, the k-space sample 
-         points
-    """
-    from scipy.fftpack import fftn, fftfreq, fftshift
-
-    n = field.shape
-    assert (n[0] == n[1] and n[0] == n[2]), 'Field should be symmetric'
-
-    # Calculate Fourier transform and components
-    field_k = fftn(field)
-    power_k = np.zeros(shape=field_k.shape, dtype=complex)
-    k = np.linspace(np.pi * n[0] / boxsize)
-
-    # Calculate the power spectrum, in a memory-efficient way
-    for i in range(n[2]):
-        power_k[:, :, i] = field_k[:, :, i] * field_k[:, :, i].conj()
-
-    power_k = np.abs(power_k)
-
-    return power_k, k
-
-
-def bin_power(power_k, k):
-    """Calculate the ensemble average of the power spectrum and bin
-    according to k
-
-    :param power_k: 
-        (array) 3D power spectrum
-    :param k:
-        (array) 1D array of k-space bin centres, assumes they are
-        linearly spaced
-    :returns: 
-        (array) power spectrum of same size as bin centres
-        (array) k-space bin centres
-    :rtype:
-
-    """
-    kx, ky, kz = np.meshgrid(k, k, k)
-
-    kk = np.sqrt(kx**2 + ky**2 + kz**2)
-    dk = abs(k[1] - k[0])
-    
-    power_k1 = np.zeros(k.shape)
-
-    for i, ik in enumerate(k):
-        dkmin = ik - dk/2
-        dkmax = ik + dk/2
-        power_k1[i] = np.mean(power_k[np.logical_and(kk > dkmin, kk <= dkmax)])
-    
-    return power_k1, k
-
-
-def power_spectrum(field, boxsize):
-    """Returns the actual power spectrum of a field.
-
-    :param field:
-        (array) field to calculate power spectrum of
-    :param boxsize: 
-        (float) length of box 
-    :returns:
-        (array) binned and normalised power spectrum of field 
-        (array) bin centres of the power spectrum
-    """
-
-    # Get the 3D power spectrum
-    power_k, k = compute_power(field, boxsize)
-    power_k, k = bin_power(power_k, k)
-
-    # Normalise power spectrum
-    power_k /= boxsize**3
-    
-    return power_k, k
---
+++ linear.py
@@ -0,0 +1,260 @@
+"""
+NOTES
+   - swapped vx/px and vy/py
+"""
+
+import sys
+import numpy as np
+import numpy.fft as fft
+from cosmology import Cosmology
+
+class Linear:
+    def __init__(self, ic, qty, params=None, approx=False, real=True):
+        """
+        Class to calculate peculiar velocities from overdensities using
+        the continuity equation
+
+        :param ic: 
+            ic_deltab file, created using grafic_tools
+        :type ic: 
+            Snapshot object, created using grafic_tools
+        :param params: 
+            either None, to get parameters from Snapshot object or 
+            string to use defined parameters
+        :type ic: 
+            None or str
+        :param approx: 
+            whether to use the approximate or full version of the 
+            continuity equation
+        :type approx: 
+            bool
+        :param real: 
+            whether to use a real FFT or regular FFT
+        :type real: 
+            bool
+        :returns: 
+        :rtype:
+
+        """
+
+        self.approx = approx
+        self.real = real
+          
+        # Set up some initial constants
+        self.z = ic.cosmo['z']     # Redshift
+        self.a = ic.cosmo['aexp']  # Scale factor
+        self.N = ic.N              # Number of samples
+        self.dx = ic.dx            # Initial grid spacing
+        self.L = ic.boxsize        # Box size in Mpc
+
+        if params is None:
+            print('Using parameters from grafic header')
+            
+            # Get parameters from grafic header
+            omega_m = ic.cosmo['omega_m']
+            omega_l = ic.cosmo['omega_l']
+            h = ic.cosmo['h']
+
+            print('---- h      :', h)
+            print('---- omega_m:', omega_m)
+            print('---- omega_l:', omega_l)
+            
+            # Collect parameters to pass to Cosmology
+            params = [omega_m, omega_l, h]
+            
+            # Calculate the relevant cosmological quantities
+            self.c = Cosmology(self.a, params=params)
+        else:
+            assert type(params).__name__ == 'str', 'To get parameters from the grafic file set params=None, otherwise specify cosmology with a string'
+
+            print('Using ' + params + ' params file')
+            self.c = Cosmology(self.a, params=params)
+
+            print('---- h      :', self.c.p['h'])
+            print('---- omega_m:', self.c.p['omega_m'])
+            print('---- omega_l:', self.c.p['omega_l'])
+            
+            
+        # Extract overdensity field and take the Fourier transform
+        self.delta_x = ic.load_box()
+
+        # Use real FFT? \delta(x) should be purely real, so should be
+        # fine to use RFFT.
+        if self.real:
+            self.delta_k = fft.rfftn(self.delta_x)
+        else:
+            self.delta_k = fft.fftn(self.delta_x)
+
+        # Calculate the FFT sample spacing
+        self.set_sample_spacing()
+        
+        # Compute the unscaled velocities
+        assert qty in ['vel', 'pos'], 'qty should be vel or pos'
+        if qty == 'vel':
+            print('Calculating velocities')
+
+            if self.approx:
+                print('---- using f(\Omega) for scaling')
+            else:
+                print('---- using full expression for scaling')
+            
+            self.set_velocity()
+            self.scale_velocity()
+            self.realise_velocity()
+
+        elif qty == 'pos':
+            print('Calculating positions')
+
+            self.set_position()
+            self.realise_position()
+
+
+    def set_sample_spacing(self):
+        """Calculates the sample spacing for a Fourier transform. If RFFT is
+        used (real=True), then the last axis contains half as many
+        components as the other two. Calculates self.k_min
+        (fundamental mode) and self.k_max (Nyquist frequency). 
+
+        kx and ky are swapped from the way you might expect since the
+        files are read and written as Fortran binary files, which are
+        column-major (as opposed to Python which is in the 'C' style,
+        row-major)
+
+        """
+
+        # Calculate the fundamental and Nyquist frequencies
+        self.k_min = 2*np.pi / self.L
+        self.k_max = np.pi / self.dx
+
+        # Sample spacing in real space
+        d = 1.0 / (self.k_min * self.N)
+
+        # Frequencies along one axis, for a full FFT
+        self.k_lin = fft.fftfreq(self.N, d=d)
+
+        # If we are using real FFTs then the thrid axis is half as big
+        if self.real:
+            # Frequencies along the half axis, for a real FFT
+            self.k_rlin = fft.rfftfreq(self.N, d=d)
+        else:
+            self.k_rlin = self.k_lin
+
+        # Generate grid of k values, for use as the vector later
+        self.ky, self.kx, self.kz = np.meshgrid(self.k_lin, self.k_lin, self.k_rlin)
+
+        # Magnitude of k values at each point
+        self.k2 = self.kx**2. + self.ky**2. + self.kz**2.
+        self.k = np.sqrt(self.k2)
+
+    
+    def set_velocity(self):
+        """
+        Calculate the common factor to the velocities i.e.
+
+        v \propto i * \delta_k * k / k^2
+
+        """
+        # Components of the velocity field without any cosmological
+        # quantities being involved
+        vx = 1j * self.delta_k * self.kx / self.k2
+        vy = 1j * self.delta_k * self.ky / self.k2
+        vz = 1j * self.delta_k * self.kz / self.k2
+
+        # Above, v(k=0) will be NaN, so convert any NaNs to zeros
+        vx = np.nan_to_num(vx)
+        vy = np.nan_to_num(vy)
+        vz = np.nan_to_num(vz)
+
+        # Return velocities
+        self.vel = [vx, vy, vz]
+
+
+    def scale_velocity(self):
+        """Scale the velocity using the approximate f(\Omega) method or using
+        the full expression as in Iliev+ (2007)
+        """
+
+        # Use f(\Omega)
+        if self.approx:
+            # Prefactor for the scaling
+            p1 = self.a * self.c.H_a * self.c.f_a
+
+            # Now calculate the scaled velocity
+            for i in range(3):
+                self.vel[i] *= p1
+    
+        # Use Eq. (10) in Iliev+ (2007)
+        else:
+            # p1 and p2 are part of d/dt(D) / D = p1*p2
+            p1 = - (3.0 * self.c.H0 * self.c.p['omega_m']) / (self.c.E_a * self.a**3.0)
+            p2 = 1 - (5.0 * self.a)/(3.0 * self.c.D_a)
+
+            # Now calculate the scaled velocity
+            for i in range(3):
+                self.vel[i] *= p1 * p2 * self.a
+
+
+    def realise_velocity(self):
+        """
+        Transform the velocity field from Fourier space to real space
+        """
+        if self.real:
+            for i in range(3):
+                self.vel[i] = fft.irfftn(self.vel[i])
+            # self.vel_r = [np.nan_to_num(v) for v in self.vel_r]
+        else:
+            for i in range(3):
+                self.vel[i] = fft.ifftn(self.vel[i]).real
+
+
+    def set_position(self):
+        """
+        Calculate the common factor to the positions i.e.
+
+        p \propto i * \delta_k * k / k^2
+
+        """
+        # Components of the velocity field without any cosmological
+        # quantities being involved
+        px = 1j * self.delta_k * self.kx / self.k2
+        py = 1j * self.delta_k * self.ky / self.k2
+        pz = 1j * self.delta_k * self.kz / self.k2
+
+        # Above, v(k=0) will be NaN, so convert any NaNs to zeros
+        px = np.nan_to_num(px)
+        py = np.nan_to_num(py)
+        pz = np.nan_to_num(pz)
+
+        # Return positions
+        self.pos = [px, py, pz]
+
+    
+    def realise_position(self):
+        """Transform the position field from Fourier space back to real space
+        """
+        if self.real:
+            for i in range(3):
+                self.pos[i] = fft.irfftn(self.pos[i])
+            # self.pos_r = [np.nan_to_num(v) for v in self.pos_r]
+        else:
+            for i in range(3):
+                self.pos[i] = fft.ifftn(self.pos[i]).real
+        
+
+def generate(ic, qty, params=None, approx=False, real=True):
+    """Convenience function for calculating the relevant
+    quantity. Returns the quantity."""
+
+    assert qty in ['vel', 'pos'], 'qty should be vel or pos'
+
+    c = Linear(ic=ic, qty=qty, params=params,
+                   approx=approx, real=real)
+    
+    if qty == 'vel':
+        return c.vel
+    
+    elif qty == 'pos':
+        assert ic.field == 'deltac', 'Should be using deltac field to calculate positions'
+        return c.pos
+
+    

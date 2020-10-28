c48ae489a695bff154dcb9896c56e0cb0077aa17
Luke Conaboy <lukeconaboy@gmail.com>
2020-10-28 11:11:00 +0000

    Added animation of IC slices

---
+++ tools/ic_anim.py
@@ -0,0 +1,46 @@
+def main(path, level, field, cmap='viridis'):
+    import numpy as np
+    import grafic_tools as grafic
+    import matplotlib.pyplot as plt
+    from matplotlib.animation import FuncAnimation
+
+    def update(i): 
+        im.set_data(x[:, :, i])
+
+    # Load data
+    x = grafic.load_snapshot(path, level, field).load_box()
+    vmin = x.min()
+    vmax = x.max()
+
+    # Set up figure
+    fig, ax = plt.subplots(figsize=(6, 6))
+    plt.axis('off')
+    
+    # Remove whitespace
+    # (https://stackoverflow.com/questions/15882395/matplotlib-animation-how-to-remove-white-margin)
+    fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
+
+    # Call imshow now, then we can just update the data later
+    im = ax.imshow(np.zeros_like(x[:, :, 0]), vmin=vmin, vmax=vmax, cmap=cmap)
+
+    # This is the main work
+    anim = FuncAnimation(fig, update, frames=np.arange(0, x.shape[2]), interval=200)
+    anim.save(field + '.gif', dpi=100, writer='imagemagick')
+
+    
+if __name__ == '__main__':
+    import sys
+    
+    if len(sys.argv) < 4:
+        print('Usage: python ic_anim.py <path> <level> <field> [<cmap>]')
+        sys.exit()
+        
+    path = sys.argv[1]
+    level = int(sys.argv[2])
+    field = sys.argv[3]
+    cmap = 'viridis'
+
+    if len(sys.argv) > 4:
+        cmap = sys.argv[4]
+
+    main(path, level, field, cmap)

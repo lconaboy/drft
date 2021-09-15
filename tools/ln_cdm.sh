f45f72274742d6f16d0c79b0b64ffd456f428fce
Luke <lukeconaboy@gmail.com>
2021-09-15 22:07:17 +0100

    Tools for linking CDM IC files

---
+++ tools/ln_cdm.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+# cd to the directory where you want to store the biased IC files
+# (e.g. simply "./biased") and set $base to be the path to the
+# unbiased directory (from the directory that contains both sets of
+# ICs, not the individual IC directory) then run this script to link
+# all of the cdm IC files
+
+# Make sure you are in the biased directory
+base=unbiased
+echo "Linking from ${base}"
+read -p "Are you in the directory containing level dirs? (y/n) " ans 
+
+fields=("ic_poscx" "ic_poscy" "ic_poscz" "ic_velcx" "ic_velcy" "ic_velcz" "ic_refmap" "ic_pvar_00001")
+# fields=("ic_pvar_00001")
+# levels=("7")
+levels=$(seq 8 13)
+
+# ln -s ../$base/level_009/ level_009
+
+if [[ $ans == "y" ]]; then
+    for level in $levels; do
+	for field in ${fields[@]}; do
+	    l=$(( $level + 1000 ))
+	    src=level_${l:1:3}
+	    cd $src
+	    ln -s ../../$base/$src/$field $field
+	    # unlink $field
+	    cd ../
+	done
+    done
+fi

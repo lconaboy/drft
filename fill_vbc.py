fc4ceaa58fa21338519c529f3f23ea666c02707b
Luke Conaboy <lukeconaboy@gmail.com>
2021-09-15 22:05:41 +0100

    Tool for filling ic_vbc with the average value of the parent cells

---
+++ fill_vbc.py
@@ -0,0 +1,25 @@
+import grafic_tools as gt
+import numpy as np
+
+levelmin = 8
+levelmax = 16
+path = '/mnt/lustre/projects/astro/general/lc589/bd/tiny_zoom/ics/unbiased/'
+base = gt.load_snapshot(path, levelmin, 'vbc')
+vbc = base.load_box()
+
+for level in range(levelmin+1, levelmax+1):
+    cur = gt.load_snapshot(path, level, 'deltab')
+    off = (np.array(cur.xoff) / base.dx).astype(int)
+    ex = (np.array(cur.n) / 2 ** (level - levelmin)).astype(int)
+
+    av_vbc = np.mean(vbc[off[0]:off[0]+ex[0],
+                         off[1]:off[1]+ex[1],
+                         off[2]:off[2]+ex[2]])
+
+    print(cur.xoff, cur.n, base.dx, off, ex)
+    cur_vbc = np.full(cur.n, av_vbc)
+    # cur.write_field(cur_vbc, 'vbc')
+
+for level in range(levelmin+1, levelmax+1):
+    cur = gt.load_snapshot(path, level, 'vbc')
+    cur.plot_slice(0)

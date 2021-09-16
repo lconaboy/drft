232b81d845a6bc5307ec714385a65d6eb0693458
Luke Conaboy <lukeconaboy@gmail.com>
2021-09-16 16:09:06 +0100

    Integrate into package, and specify overdensity to look for

--- tools/ic_average.py
+++ tools/ic_average.py
@@ -5,7 +5,7 @@ A tool to downsample an IC field and average over it.
 import numpy as np
 import matplotlib.pyplot as plt
 
-import grafic_tools as grafic
+import drft.grafic_tools as grafic
 
 def gen_fake():
     """Generates a cube of fake test data, with 8 different values. To use
@@ -45,35 +45,38 @@ def get_regions(d_tol=0.001, n_res=20):
     
 
 def downsample(path, level, field, level_s):
-    assert level_s < level, 'Sampling level must be smaller than IC level'
+    assert level_s <= level, 'Sampling level must be smaller than IC level'
 
     ics = grafic.load_snapshot(path, level, field)
-    n = 2**level      # number of cells in original cube
-    n_s = 2**level_s  # number of cells in downsampled cube
-    s = 2**(level - level_s)  # ratio of original to downsampled cells
+    if level == level_s:
+        box_s = ics.load_box()
+    else:
+        n = 2**level      # number of cells in original cube
+        n_s = 2**level_s  # number of cells in downsampled cube
+        s = 2**(level - level_s)  # ratio of original to downsampled cells
 
-    # Storage for the downsampled cube
-    box_s = np.zeros((n_s, n_s, n_s), dtype=np.float32)
+        box = ics.load_box()
+        # Storage for the downsampled cube
+        box_s = np.zeros((n_s, n_s, n_s), dtype=np.float32)
 
-    box = ics.load_box()
-    for ii in range(n_s):
-        for jj in range(n_s):
-            for kk in range(n_s):
-                tmp = np.zeros(s**3)
+        for ii in range(n_s):
+            for jj in range(n_s):
+                for kk in range(n_s):
+                    tmp = np.zeros(s**3)
                 
-                # Check whether we've looped around
-                iis = np.arange(ii*s, ii*s + s)
-                jjs = np.arange(jj*s, jj*s + s)
-                kks = np.arange(kk*s, kk*s + s)
+                    # Check whether we've looped around
+                    iis = np.arange(ii*s, ii*s + s)
+                    jjs = np.arange(jj*s, jj*s + s)
+                    kks = np.arange(kk*s, kk*s + s)
 
-                l = 0
-                for iii in iis:
-                    for jjj in jjs:
-                        for kkk in kks:
-                            tmp[l] = box[iii, jjj, kkk]
-                            l += 1
+                    l = 0
+                    for iii in iis:
+                        for jjj in jjs:
+                            for kkk in kks:
+                                tmp[l] = box[iii, jjj, kkk]
+                                l += 1
                             
-                box_s[ii, jj, kk] = np.mean(tmp)
+                    box_s[ii, jj, kk] = np.mean(tmp)
 
     return box_s
 
@@ -91,48 +94,50 @@ def get_cen(pp, n):
 # level = 7
 # level_s = 4  # level to downsample to, i.e. n_samp = 2**level_s
 
-path = './'
-level = 9
-level_s = 6
+if __name__ == '__main__':
+    path = './'
+    level = 8
+    level_s = 7
 
-ics = grafic.load_snapshot(path, level, 'deltab')
-
-fac = 1001. * ics.cosmo['aexp']  # scale v_bc to z=1000
-
-d_tol = 0.001  # how close to mean density?
-n_res = 20     # how many regions?
-
-# Load up downsampled boxes
-box = ics.load_box()
-d_box = downsample(path, level, 'deltab', level_s)
-v_box = downsample(path, level, 'vbc', level_s)
-
-# Density contrast smaller than some specified value
-d_idx = np.argwhere(np.abs(d_box) < d_tol)
-
-print('---- found {0} regions with |\delta| < {1}'.format(d_idx.shape[0], d_tol))
-
-# Find largest v_bc
-v_idx = d_idx[np.argsort(v_box[d_idx[:, 0], d_idx[:, 1], d_idx[:, 2]]), :]
-
-h = '-------- v_bc (km/s) | \delta'
-s = '         {0:>7.5}     | {1:>7.5}'
-print(h)
-
-out = np.zeros((n_res, 5))
-j = 0
-
-# Sorts are done in ascending order, so read backwards
-for i in range(-1, -1 - n_res, -1):
-    v_i = v_box[v_idx[i, 0], v_idx[i, 1], v_idx[i, 2]]
-    d_i = d_box[v_idx[i, 0], v_idx[i, 1], v_idx[i, 2]]
-    cc = get_cen([v_idx[i, 0], v_idx[i, 1], v_idx[i, 2]], 2**level)
-
-    print(s.format(v_i*fac, d_i))
+    ics = grafic.load_snapshot(path, level, 'deltab')
     
-    out[j, 0] = v_i*fac
-    out[j, 1] = d_i
-    out[j, 2:] = cc
-    j += 1
+    fac = 1001. * ics.cosmo['aexp']  # scale v_bc to z=1000
+
+    d_val = -0.01
+    d_tol = 0.001  # how close to this density?
+    n_res = 20     # how many regions?
+
+    # Load up downsampled boxes
+    box = ics.load_box()
+    d_box = downsample(path, level, 'deltab', level_s)
+    v_box = downsample(path, level, 'vbc', level_s)
+
+    # Density contrast smaller than some specified value
+    d_idx = np.argwhere(np.abs(d_box - d_val) < d_tol)
+
+    print('---- found {0} regions with |\delta| < {1}'.format(d_idx.shape[0], d_tol))
+
+    # Find largest v_bc
+    v_idx = d_idx[np.argsort(v_box[d_idx[:, 0], d_idx[:, 1], d_idx[:, 2]]), :]
+
+    h = '-------- v_bc (km/s) | \delta'
+    s = '         {0:>7.5}     | {1:>7.5}'
+    print(h)
+
+    out = np.zeros((n_res, 5))
+    j = 0
+
+    # Sorts are done in ascending order, so read backwards
+    for i in range(-1, -1 - n_res, -1):
+        v_i = v_box[v_idx[i, 0], v_idx[i, 1], v_idx[i, 2]]
+        d_i = d_box[v_idx[i, 0], v_idx[i, 1], v_idx[i, 2]]
+        cc = get_cen([v_idx[i, 0], v_idx[i, 1], v_idx[i, 2]], 2**level)
+
+        print(s.format(v_i*fac, d_i))
     
-np.savetxt('out.txt', out, fmt='%10.5f', header='v_bc,rec (km/s) \delta x y z')
+        out[j, 0] = v_i*fac
+        out[j, 1] = d_i
+        out[j, 2:] = cc
+        j += 1
+    
+    np.savetxt('out.txt', out, fmt='%10.12f', header='v_bc,rec (km/s) \delta x y z')

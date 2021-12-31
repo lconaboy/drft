999e9f02a59f085c2fd9f8a80a0cbfd68e423ed1
Luke Conaboy <lukeconaboy@gmail.com>
2021-12-31 20:36:45 +0000

    yt_ic_vbc correctly interpolates undersampled v_bc (problem for the finer levels)

--- yt_ic_vbc.py
+++ yt_ic_vbc.py
@@ -38,33 +38,64 @@ def yt_vbc(unbiased_path, ic_path):
     # Do levelmin first. Could be done inside the loop, but I had it
     # outside from when I was plotting vbc slices.
     s = load_snapshot(ic_path, ds['levelmin'], 'deltab')
-
-    ad = ds.covering_grid(level=0, left_edge=[0, 0, 0],
+    left_edge = np.array([0, 0, 0])  # tmp
+    ad = ds.covering_grid(level=0, left_edge=left_edge,
                           dims=ds.domain_dimensions)
 
     u = 'km/s'
     vbc = vbc_calc(ad, u)
-    print('min/max/avg', vbc.min(), vbc.max(), vbc.mean())
+    # print('min/max/avg', vbc.min(), vbc.max(), vbc.mean())
     s.write_field(vbc, 'vbc')
     
-    vmin = vbc.min()
-    vmax = vbc.max()
+    # vmin = vbc.min()
+    # vmax = vbc.max()
     # plot_vbc_slices(vbc, levelmin, vmin, vmax)
     
     # Now do the other levels. To find out the actual levelmin we
     # would have to load up all the particle masses and do a min/max,
     # which is quite expensive. Instead, we just go until we run out
     # of files.
+    # for ilevel in range(levelmin+1, levelmax+1):
     for ilevel in range(levelmin+1, levelmax+1):
         try:
             s = load_snapshot(ic_path, ilevel, 'deltab')
             dims = s.n
-            left_edge = (s.xoff / s.dx) / 2**ilevel
-            ad = ds.covering_grid(ilevel-levelmin, left_edge=left_edge,
-                                  dims=dims)
-            vbc = vbc_calc(ad, u)
+            left_edge_new = (s.xoff / s.dx) / 2**ilevel
+            right_edge_new = ((s.xoff / s.dx) + dims) / 2**ilevel
+            # ad = ds.smoothed_covering_grid(ilevel-levelmin,
+            #                                left_edge=left_edge, dims=dims)
+            ad = ds.arbitrary_grid(left_edge=left_edge_new,
+                                   right_edge=right_edge_new,
+                                   dims=dims)
+
+            vbc_new = vbc_calc(ad, u)
+
+            if res_fix:
+                # Array of indices with undersamapled velocities
+                res_new = np.argwhere(vbc_new > res_fix)
+
+                if len(res_new) > 0:
+                    print(f'-- resampling velocities over {res_fix:.2f} for level {ilevel:d}')
+                    # Indices in ilevel - 1
+                    res_old = ((left_edge_new - left_edge) *
+                               2**ilevel + res_new) / 2
+                    res_old = res_old.astype(int)
+
+                    # FIXME not working
+                    for i in range(res_new.shape[0]):
+                        new = res_new[i, :]
+                        old = res_old[i, :]
+                        print(new, old)
+                        vbc_new[new[0], new[1], new[2]] = vbc[old[0],
+                                                              old[1],
+                                                              old[2]]
+                    
             # print('min/max/avg', vbc.min(), vbc.max(), vbc.mean())
+            vbc = vbc_new
+            left_edge = left_edge_new
+            right_edge = right_edge_new
             s.write_field(vbc, 'vbc')
+
 
             # plot_vbc_slices(vbc, ilevel, vmin, vmax)
             
@@ -105,5 +136,9 @@ if __name__ == '__main__':
     levelmin = int(sys.argv[3])
     levelmax = int(sys.argv[4])
 
+    # We aren't going to find vbc_recom > 80 km/s in this box size, so
+    # this indicates undersamapled velocities
+    res_fix = 16
+    
     yt_vbc(unbiased_path, ic_path)
-    grafic_vbc(unbiased_path, levelmin, levelmax)
+    #grafic_vbc(unbiased_path, levelmin, levelmax)
